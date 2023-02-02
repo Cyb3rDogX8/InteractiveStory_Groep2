cc99d7259262413bc6e6da0d6a7bec0a7025f444
mmdmcy <112551618+mmdmcy@users.noreply.github.com>
2023-02-02 17:35:52 +0100

    Dialogue Scripts
    Best wel veel scripts, geen charactermovement script erbij.

--- Assets/PREFAB BEAU/PACKAGE SAVE/DialogueTrigger.cs
+++ Assets/PREFAB BEAU/PACKAGE SAVE/DialogueTrigger.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class DialogueTrigger : Dialogue
-{
-    public string GivenText;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            CreateDialogueText(GivenText);
-        }
-    }
-}
+//public class DialogueTrigger : Dialogue
+//{
+//    public string GivenText;
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+ //           CreateDialogueText(GivenText);
+ //       }
+ //   }
+//}
